886f65e43e5f3a98e80e83b6964e4622d1511bea
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-09-15 21:29:38 -0300

    feat: add register user

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(
+    @Body() registerRequest: { name: string; password: string; email: string },
+  ) {
+    return await this.authService.registerUser(registerRequest);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+} from 'amazon-cognito-identity-js';
+import { AuthConfig } from './auth.config';
+
+@Injectable()
+export class AuthService {
+  private userPool: CognitoUserPool;
+
+  constructor(
+    @Inject(AuthConfig)
+    private readonly authConfig: AuthConfig,
+  ) {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: this.authConfig.userPoolId,
+      ClientId: this.authConfig.clientId,
+    });
+  }
+
+  registerUser(registerRequest: {
+    name: string;
+    email: string;
+    password: string;
+  }) {
+    const { name, email, password } = registerRequest;
+    return new Promise((resolve, reject) => {
+      return this.userPool.signUp(
+        email,
+        password,
+        [
+          new CognitoUserAttribute({ Name: 'name', Value: name }),
+          new CognitoUserAttribute({ Name: 'email', Value: email }),
+        ],
+        null,
+        (err, result) => {
+          if (!result) {
+            reject(err);
+          } else {
+            resolve(result.user);
+          }
+        },
+      );
+    });
+  }
+}
